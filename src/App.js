8e107f72d5397bd36582c2c1e754c718e692b957
smtsuchi <shoha1999@gmail.com>
2022-03-31 21:21:35 -0400

    thursday

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import About from './views/About';
 import Contact from './views/Contact';
 import Home from './views/Home';
 import IG from './views/IG';
+import Login from './views/Login';
 import News from './views/News';
 import Shop from './views/Shop';
+import SignUp from './views/SignUp';
 import SinglePost from './views/SinglePost';
 import SingleProduct from './views/SingleProduct';
 
@@ -15,10 +17,17 @@ export default class App extends Component {
     super();
     // console.log('MAIN APP: i have constructed')
     this.state = {
-      name: 'shoha',
       age: 9000,
-      people: ['andrew', 'david', 'anthony', 'tyler', 'tommy', 'april', 'christopher', 'dylan', 'jamia']
+      people: ['andrew', 'david', 'anthony', 'tyler', 'tommy', 'april', 'christopher', 'dylan', 'jamia'],
+      user: {}
     }
+  }
+
+  logMeIn = (userObj) => {
+    this.setState({user: userObj})
+  }
+  logMeOut = () => {
+    this.setState({user: {}})
   }
 
   happyBirthday = () => {
@@ -40,7 +49,7 @@ export default class App extends Component {
     // console.log("MAIN APP: I have rendered")
     return (
       <div>
-        <Navbar myName={this.state.name} />
+        <Navbar currentUser={this.state.user} logMeOut={this.logMeOut} />
         {/* <h1>Hi, I am {this.state['name']} and my age is {this.state.age}</h1>
         <button onClick={()=>this.happyBirthday()}>Happy Birthday</button> */}
         <div className='container d-flex justify-content-center mt-4'>
@@ -53,6 +62,9 @@ export default class App extends Component {
             <Route path='/shop' element={<Shop />} />
             <Route path='/shop/:productId' element={<SingleProduct />} />
             <Route path='/contact' element={<Contact />} />
+
+            <Route path='/login' element={<Login logMeIn={this.logMeIn}/>} />
+            <Route path='/signup' element={<SignUp />} />
           </Routes>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css'
 export default class Navbar extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light my-nav">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light my-nav">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,23 +31,26 @@ export default class Navbar extends Component {
               <li className="nav-item">
                 <Link className="nav-link" to="/shop">Shop</Link>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
-                </ul>
-              </li>
+              {this.props.currentUser.username ?
+                <li className="nav-item" onClick={()=>this.props.logMeOut()}>
+                  <Link className="nav-link" to="/login">Log Out</Link>
+                </li>
+                :
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                  </li>
+                </>
+              }
               <li className="nav-item">
-                <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">{this.props.myName}</a>
+                <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">{this.props.currentUser.username}</a>
               </li>
             </ul>
             <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
           </div>
